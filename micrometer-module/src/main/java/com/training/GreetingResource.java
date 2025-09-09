15b8a90738c83ebd0b0801ec4c5e9102ee3dcfcf
axodevelopment <axodevelopment@icloud.com>
2025-09-09 03:44:32 -0400

    updating some micrometer basics

--- micrometer-module/src/main/java/com/training/GreetingResource.java
+++ micrometer-module/src/main/java/com/training/GreetingResource.java
@@ -1,16 +1,37 @@
 package com.training;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
+import io.micrometer.core.instrument.Timer;
+
+import io.micrometer.core.annotation.Timed;
+
+import org.jboss.logging.Logger;
+
 @Path("/hello")
 public class GreetingResource {
+
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
+    @Inject
+    MeterRegistry registry;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("hello: OLTP");
+        registry.counter("micrometer.counter", "type", "add").increment();
         return "Hello from Quarkus REST";
+    }
+
+    @Timed(value = "micrometer.greeting.timer")
+    public String timer() {
+        return "OK";
     }
 }
